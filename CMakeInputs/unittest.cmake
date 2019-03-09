207bfdc30bb3e2871a11d1cbdcbd92c9b42f550b
Xinyu Ma <bitmxy@gmail.com>
2019-03-09 09:24:11 -0800

    Update branches

--- CMakeInputs/unittest.cmake
+++ CMakeInputs/unittest.cmake
@@ -67,3 +67,10 @@ target_sources(unittest PRIVATE
   "${DIR_UNITTESTS}/sign-verify/ecdsa-sign-verify-tests/test-secp256k1-def.h"
   "${DIR_UNITTESTS}/sign-verify/ecdsa-sign-verify-tests/test-secp256k1-def.c"
 )
+
+#target_sources(unittest PRIVATE
+#  "${DIR_UNITTESTS}/schematized-trust/trust-schema-tests.h"
+#  "${DIR_UNITTESTS}/schematized-trust/trust-schema-tests.c"
+#  "${DIR_UNITTESTS}/schematized-trust/trust-schema-tests-def.h"
+#  "${DIR_UNITTESTS}/schematized-trust/trust-schema-tests-def.c"
+#)
---
+++ adaptation/udp-unicast/ndn-udp-unicast-face.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2019 Xinyu Ma
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v3.0. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef NDN_UDP_UNICAST_FACE_H_
+#define NDN_UDP_UNICAST_FACE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
