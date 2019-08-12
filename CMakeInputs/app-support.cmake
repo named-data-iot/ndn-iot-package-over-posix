78d51466df4d5796b8de03e59692f9622f0b8a19
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2019-08-12 15:12:21 -0700

    update to new ndn-lite and riot test

--- CMakeInputs/app-support.cmake
+++ CMakeInputs/app-support.cmake
@@ -6,12 +6,15 @@ set(DIR_ECC256 "${DIR_VARIANTS}/ecc_256")
 target_sources(ndn-lite PUBLIC
   ${DIR_APP_SUPPORT}/access-control.h
   ${DIR_APP_SUPPORT}/service-discovery.h
+  ${DIR_APP_SUPPORT}/security-bootstrapping.h
+  ${DIR_APP_SUPPORT}/ndn-sig-verifier.h
 )
 target_sources(ndn-lite PRIVATE
   ${DIR_APP_SUPPORT}/access-control.c
   ${DIR_APP_SUPPORT}/service-discovery.c
   ${DIR_APP_SUPPORT}/security-bootstrapping.c
-  ${DIR_APP_SUPPORT}/security-bootstrapping.h
+  ${DIR_APP_SUPPORT}/ndn-sig-verifier.c
+  ${DIR_APP_SUPPORT}/ndn-sig-verifier.c
   ${DIR_SECURE_SIGN_ON}/sign-on-basic-client-consts.h
   ${DIR_SECURE_SIGN_ON}/sign-on-basic-client.c
   ${DIR_SECURE_SIGN_ON}/sign-on-basic-client.h
