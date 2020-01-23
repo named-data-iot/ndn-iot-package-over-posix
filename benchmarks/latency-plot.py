c74ceb1c4f0ac34a6c0305e35417e0423630efdb
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2020-01-22 23:45:19 -0800

    update benchmark results

--- benchmarks/latency-plot.py
+++ benchmarks/latency-plot.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-labels = ['Bootstrapping', 'Sub:\nContent Fetching', 'Pub:\nCommand Publish']
+labels = ['Bootstrapping', 'Sub:\nContent Delievery', 'Pub:\nCommand Delivery']
 laptop_means = []
 laptop_stds = []
 pi_means = []
 pi_stds = []
+aws_means = []
+aws_stds = []
 
 def parse_results():
     tmp = []
@@ -50,10 +52,33 @@ def parse_results():
     pi_means.append(np.mean(tmp))
     pi_stds.append(np.std(tmp))
 
+    tmp = []
+    with open('bootstrapping/latency/aws-iot-tls-setup.dat') as file:
+        for line in file:
+            tmp.append(int(line.strip()[:-2]))
+    aws_means.append(np.mean(tmp))
+    aws_stds.append(np.std(tmp))
+
+    tmp = []
+    with open('pub-sub/latency/aws-iot-content-delivery.dat') as file:
+        for line in file:
+            tmp.append(int(line.strip()))
+    aws_means.append(np.mean(tmp))
+    aws_stds.append(np.std(tmp))
+
+    tmp = []
+    with open('pub-sub/latency/aws-iot-cmd-delivery.dat') as file:
+        for line in file:
+            tmp.append(int(line.strip()))
+    aws_means.append(np.mean(tmp))
+    aws_stds.append(np.std(tmp))
+
     print(pi_means)
     print(pi_stds)
     print(laptop_means)
     print(laptop_stds)
+    print(aws_means)
+    print(aws_stds)
 
 
 parse_results()
@@ -61,11 +86,12 @@ parse_results()
 x = np.arange(len(labels))  # the label locations
 width = 0.3  # the width of the bars
 
-fig, ax = plt.subplots(figsize=(5, 3.5))
+fig, ax = plt.subplots(figsize=(6, 4))
 patterns = [ "///" , "..", "xx", "ooo", "\\\\"]
 
-rects1 = ax.bar(x - width/2, laptop_means, width, yerr=laptop_stds, hatch=patterns[0], label='Unix Socket\ncore i7 2.2GHz', edgecolor='gray')
-rects2 = ax.bar(x + width/2, pi_means, width, yerr=pi_stds, hatch=patterns[1], label='WiFi UDP Multicast\ncortex A53 1.4GHz', edgecolor='gray')
+rects1 = ax.bar(x - width, laptop_means, width, yerr=laptop_stds, hatch=patterns[0], label='NDN-LITE on Unix Socket\nApp host:core i7 2.2GHz', edgecolor='gray')
+rects2 = ax.bar(x, pi_means, width, yerr=pi_stds, hatch=patterns[1], label='NDN-LITE on WiFi UDP Multicast\nDevice:cortex A53 1.4GHz', edgecolor='gray')
+rects3 = ax.bar(x + width, aws_means, width, yerr=aws_stds, hatch=patterns[2], label='AWS IoT on WiFi TLS\nDevice:core i7 2.2GHz', edgecolor='gray')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Total Latency (ms)')
@@ -87,6 +113,7 @@ def autolabel(rects):
 
 autolabel(rects1)
 autolabel(rects2)
+autolabel(rects3)
 
 fig.tight_layout()
 fig.tight_layout()
